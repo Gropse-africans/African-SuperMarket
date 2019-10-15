42352b07b2e531c00c48f45586a9f3ce6352984f
Nitesh <nitesh@Niteshs-MacBook-Air.local>
2019-10-15 10:52:57 +0530

    Initial Commit

---
+++ AfricanSuperMarket/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AfricanSuperMarket
+//
+//  Created by Nitesh on 15/10/19.
+//  Copyright © 2019 Nitesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
